b5b111df1b0170a0ae30ad64cbc679fdad2e208e
Matt Hamilton <matt@enquos.com>
2016-12-13 12:42:38 +0000

    initial commit. Password needs removing from app.yaml

--- main.py
+++ main.py
@@ -1,25 +1,108 @@
-"""`main` is the top level module for your Flask application."""
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Import the Flask Framework
-from flask import Flask
-app = Flask(__name__)
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+"""
+Sample App Engine application demonstrating how to connect to Google Cloud SQL
+using App Engine's native unix socket or using TCP when running locally.
+
+For more information, see the README.md.
+"""
+
+# [START all]
+import os
+import random
+
+import MySQLdb
+import webapp2
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD,
+            db='freeicecream')
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
 
 
-@app.errorhandler(500)
-def application_error(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        """Simple request handler that shows all of the MySQL variables."""
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('SELECT id,node,owner FROM bank')
+
+        for r in cursor.fetchall():
+            self.response.write('{0[0]:32s} {0[1]:5s} {0[2]:20s}\n'.format(r))
+
+class Tick(webapp2.RequestHandler):
+    def get(self):
+        """ do a tick of the game logic """
+
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute('SELECT id,leak FROM node')
+
+        for r in cursor.fetchall():
+            id, leak = r
+            if random.random() < leak:
+                # delete the row
+                cursor2 = db.cursor()
+                cursor2.execute('DELETE FROM bank WHERE node=%s ORDER BY rand() LIMIT 1', (id,))
+                db.commit()
+                self.response.write('deleted {}\n'.format(id))
+
+        cursor3 = db.cursor()
+        cursor3.execute('SELECT id,node,owner FROM bank')
+
+        for r in cursor3.fetchall():
+            self.response.write('{0[0]:32s} {0[1]:5s} {0[2]:20s}\n'.format(r))
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/tick', Tick),
+], debug=True)
+
+# [END all]
