fbd116ed80a98271b800102a0d031e6b189880e2
Matt Hamilton <matt@enquos.com>
2017-01-10 15:01:10 +0000

    Add enpoint to get specific player

--- gameserver/api/endpoints/players.py
+++ gameserver/api/endpoints/players.py
@@ -7,7 +7,7 @@ from gameserver.game import Game
 from gameserver.models import Player
 
 from gameserver.api.restplus import api
-from gameserver.api.serializers import player
+from gameserver.api.serializers import player_get, player_post
 
 from gameserver.database import db
 
@@ -22,7 +22,8 @@ game = Game()
 @ns.route('/')
 class PlayerCollection(Resource):
 
-    @api.marshal_list_with(player)
+    @api.response(200, 'Success')
+    @api.marshal_list_with(player_get)
     def get(self):
         """
         Returns list of players.
@@ -31,7 +32,7 @@ class PlayerCollection(Resource):
         return players
 
     @api.response(201, 'Player successfully created.')
-    @api.expect(player)
+    @api.expect(player_post)
     def post(self):
         """
         Creates a new game player.
@@ -40,6 +41,23 @@ class PlayerCollection(Resource):
         player = game.create_player(data['name'])
         db_session.commit()
         return player.id, 201
+
+
+@ns.route('/<string:id>')
+@ns.param('id', 'The player id')
+class Player(Resource):
+
+    @api.response(404, 'Player not found')
+    @api.response(200, 'Success')
+    @api.marshal_with(player_get)
+    def get(self, id):
+        """                                                                                                                                                                            
+        Returns the specified player.                                                                                                                                                       
+        """
+        player = game.get_player(id)
+        if not player:
+            api.abort(404)
+        return player
 
 
 
--- gameserver/api/serializers.py
+++ gameserver/api/serializers.py
@@ -2,8 +2,16 @@
 from flask_restplus import fields
 from gameserver.api.restplus import api
 
-player = api.model('Player', {
+player_get = api.model('Player GET', {
     'id': fields.String(readOnly=True, description='The unique identifier of a player'),
     'name': fields.String(required=True, description='Player name'),
+})
+
+player_post = api.model('Player POST', {
+    'name': fields.String(required=True, description='Player name'),
+})
+
+player_delete = api.model('Player DELETE', {
+    'id': fields.String(readOnly=True, description='The unique identifier of a player'),
 })
 
