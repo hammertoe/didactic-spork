85acc24c9d8e918d1aef6d6ba738d18811c01a82
Matt Hamilton <matt@enquos.com>
2017-01-22 09:44:41 -0800

    standardise on using player_id as attribute when we need it. Makes the auth decorator easier

--- gameserver/controllers/players_controller.py
+++ gameserver/controllers/players_controller.py
@@ -21,8 +21,8 @@ def player_to_dict(player):
                 table=player.table_id,
                 )
 
-def get_player(id):
-    player = game.get_player(id)
+def get_player(player_id):
+    player = game.get_player(player_id)
     if not player:
         return "Player not found", 404
     return player_to_dict(player), 200
@@ -41,13 +41,13 @@ def create_player(player=None):
     else:
         return 500
 
-def update_player(id, player=None):
+def update_player(player_id, player=None):
     """
     Updates a player
     """
     if not player:
         return 500
-    p = game.get_player(id)
+    p = game.get_player(player_id)
     if not p:
         return "Player not found", 404
 
@@ -59,21 +59,21 @@ def update_player(id, player=None):
     db_session.commit()
     return player_to_dict(p), 200
 
-def set_funding(id, funding = None):
+def set_funding(player_id, funding = None):
     try:
-        game.set_funding(id, funding)
+        game.set_funding(player_id, funding)
         db_session.commit()
-        return game.get_funding(id), 200
+        return game.get_funding(player_id), 200
     except ValueError:
         return "Sum of funds exceeds max allowed", 400
 
-def get_funding(id):
-    funds = game.get_funding(id)
+def get_funding(player_id):
+    funds = game.get_funding(player_id)
 
     return funds, 200
 
-def add_policy(id, policy):
-    game.add_fund(id, policy, 0.0)
+def add_policy(player_id, policy):
+    game.add_fund(player_id, policy, 0.0)
     db_session.commit()
 
 def get_policy_offer(player_id, policy_id):
